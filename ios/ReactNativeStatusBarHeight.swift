1c09c8fa2022600df67c4cf5472a937d9ec38a71
samarth-sheth-us <samarth.sheth@logicwind.com>
2025-03-05 16:49:45 +0530

    Initialize package

---
+++ ios/ReactNativeStatusBarHeight.swift
@@ -0,0 +1,8 @@
+@objc(ReactNativeStatusBarHeight)
+class ReactNativeStatusBarHeight: NSObject {
+
+  @objc(multiply:withB:withResolver:withRejecter:)
+  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+    resolve(a*b)
+  }
+}
